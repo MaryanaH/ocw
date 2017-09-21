3ea6241c0bfcec6dab71d1f5821f93965ed3e00e
Maryana <test@test.st>
2017-09-21 23:00:06 +0300

    Fix comments

--- src/main/java/com/mvelyka/marathon/Performer.java
+++ src/main/java/com/mvelyka/marathon/Performer.java
@@ -1,37 +1,18 @@
 package com.mvelyka.marathon;
 
-public class Performer implements Comparable<Performer>{
+import lombok.*;
+
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class Performer implements Comparable<Performer> {
+
     private String name;
     private int score;
-
-    public Performer(String name, int score) {
-        this.name = name;
-        this.score = score;
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
 
     @Override
     public int compareTo(Performer o) {
         return (this.score - o.score);
-    }
-
-    public String toString() {
-        return this.name;
     }
 }
--- src/test/java/MarathonTest.java
+++ src/test/java/MarathonTest.java
@@ -5,24 +5,26 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class MarathonTest {
-    Performer[] performerList = new Performer[3];
-    private Marathon marathon = new Marathon(performerList);
+    Performer[] performers;
+    private Marathon marathon = new Marathon(performers);
 
     @Test
     public void bestPerformerTest() {
-        performerList[0] = new Performer("name1", 5);
-        performerList[1] = new Performer("name2", 4);
-        performerList[2] = new Performer("name3", 6);
+        performers = new Performer[3];
+        performers[0] = new Performer("name1", 5);
+        performers[1] = new Performer("name2", 4);
+        performers[2] = new Performer("name3", 6);
         Performer actual = marathon.bestPerformer();
-        assertEquals(performerList[2], actual);
+        assertEquals(performers[2], actual);
     }
 
     @Test
     public void secondBestPerformerTest() {
-//        performerList[0] = new Performer("name1", 5);
-//        performerList[1] = new Performer("name2", 4);
-//        performerList[2] = new Performer("name3", 6);
+        performers = new Performer[3];
+        performers[0] = new Performer("name1", 5);
+        performers[1] = new Performer("name2", 4);
+        performers[2] = new Performer("name3", 6);
         Performer actual = marathon.secondBestPerformer();
-        assertEquals(performerList[0], actual);
+        assertEquals(performers[0], actual);
     }
 }
