ea80c614754668ee90545c658184be0571437427
Maryana <test@test.st>
2017-10-03 22:26:50 +0300

    Magic Square implementation with tests

---
+++ src/main/java/com/mvelyka/magicSquares/IFigure.java
@@ -0,0 +1,10 @@
+package com.mvelyka.magicSquares;
+
+/**
+ * Created by mvelyka on 9/29/17.
+ */
+public class IFigure {
+    public void figureType() {
+        System.out.println("Hello I'm a figure");
+    }
+}
